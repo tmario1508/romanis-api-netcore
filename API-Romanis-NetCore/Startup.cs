0b90f24f19032ad1dcfe915b26bbb0abdfdc7402
Jose Ignacio Vaqueiro Gonzalez <ignaciovaqdevelop@hotmail.com>
2020-11-30 01:23:58 -0600

    Implementacio de permisos CORS

--- API-Romanis-NetCore/Startup.cs
+++ API-Romanis-NetCore/Startup.cs
@@ -25,6 +25,15 @@ namespace API_Romanis_NetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                       .AllowAnyHeader()
+                       .AllowAnyMethod();
+                });
+            });
             services.AddControllers();
         }
 
@@ -37,6 +46,8 @@ namespace API_Romanis_NetCore
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("EnableCORS");
 
             app.UseRouting();
 
